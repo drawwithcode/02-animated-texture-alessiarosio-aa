0dc9a09afcd6efd0de8d3572c26537b892cef1d3
alessiaarosio <alessiarosio.aa@gmail.com>
2020-10-13 11:40:58 +0200

    Update sketch.js
    creazione di una texture

--- sketch.js
+++ sketch.js
@@ -3,10 +3,28 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
+  createCanvas(windowWidth,windowHeight);
+  background("black");
+  colorMode(HSB, 100);
+  angleMode(DEGREES);
   // put setup code here
 }
 
 function draw() {
+
+  for (let y = 0; y <= noise(frameCount) * windowHeight; y +=15)
+  {
+   push();
+   fill(100*mouseX/windowWidth, 30, 70, random()*100);
+   noStroke();
+   ellipse(frameCount, y, random(10));
+   pop();
+  }
+
+  textSize(18);
+  text('Move the mouse to change color', 630, 690);
+  fill("white");
+  noStroke();
+
   // put drawing code here
 }
